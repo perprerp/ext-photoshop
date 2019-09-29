3c8ce44b39b59a07666d5737131e5af10f7bba34
perprerp <7416496+perprerp@users.noreply.github.com>
2019-09-29 02:55:10 +0100

    Delete per3d_export_to_path.jsx

--- folder/per3d_export_to_path.jsx
+++
@@ -1,9 +0,0 @@
-﻿#include per3d_exporter.jsxinc
-
-//===========================================================================
-// per3d Photoshop Exporter
-// 
-//   Please see "per3d_photoshop_exporter.txt" for complete documentation.
-//===========================================================================
-
-PER3D_export ( null, "/c/mygame/assets/shoes");
