31361e1b98fd7538760fa8feba9f6f3961ce0782
perprerp <7416496+perprerp@users.noreply.github.com>
2019-09-29 02:57:21 +0100

    Add files via upload
    photoshop image exporter

---
+++ per3d_photoshop_exporter/per3d_export_to_treesync.jsx
@@ -0,0 +1,9 @@
+﻿#include per3d_exporter.jsxinc
+
+//===========================================================================
+// per3d Photoshop Exporter
+// 
+//   Please see "per3d_photoshop_exporter.txt" for complete documentation.
+//===========================================================================
+
+PER3D_export ( "/c/thegame/", "/n/projects/thegame");	// Remember that these point to the project root folders, not the asset folder.
