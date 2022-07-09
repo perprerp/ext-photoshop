b1e0555e5fb9c29cef843795d416a8d10c9242f2
perprerp <7416496+perprerp@users.noreply.github.com>
2022-07-10 00:04:15 +0100

    Update and rename per3d_photoshop_exporter/per3d_export_to_subfolder.jsx to exporter/per_export_to_subfolder.jsx

---
+++ exporter/per_export_to_subfolder.jsx
@@ -0,0 +1,9 @@
+﻿#include per_exporter.jsxinc
+
+//===========================================================================
+// Photoshop Exporter - by perprerp
+// 
+//   Please see "per_photoshop_exporter.txt" for complete documentation.
+//===========================================================================
+
+PER_export ( null, "subfolder_name");	// Can only be one folder deep.
