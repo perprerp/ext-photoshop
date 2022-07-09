a77293475b7275e6bc4f92bbef005911f7ff11b5
perprerp <7416496+perprerp@users.noreply.github.com>
2022-07-10 00:04:58 +0100

    Update and rename per3d_photoshop_exporter/per3d_export_to_treesync.jsx to exporter/per_export_to_treesync.jsx

--- exporter/per_export_to_treesync.jsx
+++ exporter/per_export_to_treesync.jsx
@@ -1,9 +1,9 @@
-﻿#include per3d_exporter.jsxinc
+﻿#include per_exporter.jsxinc
 
 //===========================================================================
-// per3d Photoshop Exporter
+// Photoshop Exporter - by perprerp
 // 
 //   Please see "per3d_photoshop_exporter.txt" for complete documentation.
 //===========================================================================
 
-PER3D_export ( "/c/thegame/", "/n/projects/thegame");	// Remember that these point to the project root folders, not the asset folder.
+PER_export ( "/c/thegame/", "/n/projects/thegame");	// Remember that these point to the project root folders, not the asset folder.
