51f3ba0cbbbdc4c4190285d700b6259a6f11fbc7
perprerp <7416496+perprerp@users.noreply.github.com>
2022-07-10 00:05:36 +0100

    Update per_export_to_treesync.jsx

--- exporter/per_export_to_treesync.jsx
+++ exporter/per_export_to_treesync.jsx
@@ -3,7 +3,7 @@
 //===========================================================================
 // Photoshop Exporter - by perprerp
 // 
-//   Please see "per3d_photoshop_exporter.txt" for complete documentation.
+//   Please see "per_photoshop_exporter.txt" for complete documentation.
 //===========================================================================
 
 PER_export ( "/c/thegame/", "/n/projects/thegame");	// Remember that these point to the project root folders, not the asset folder.
