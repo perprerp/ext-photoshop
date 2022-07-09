2f7f85769ff2c9887eec16762a0cbf1510ca890f
perprerp <7416496+perprerp@users.noreply.github.com>
2022-07-10 00:09:59 +0100

    Update and rename per3d_export_to_current.jsx to per_export_to_current.jsx

---
+++ exporter/per_export_to_current.jsx
@@ -0,0 +1,9 @@
+﻿#include per_exporter.jsxinc
+
+//===========================================================================
+// Photoshop Exporter - by perprerp
+// 
+//   Please see "per_photoshop_exporter.txt" for complete documentation.
+//===========================================================================
+
+PER_export ( null, null);
